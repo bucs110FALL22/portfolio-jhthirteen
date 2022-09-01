874f533d1794e70f7c69070b1f1da99186cf71a5
Jack Hunter <89477914+jhthirteen@users.noreply.github.com>
2022-09-01 16:53:10 +0000

    Started ex)1

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,6 @@
+#the * operator is multiplication 
+print("In python, 10 * 5 = ", 10*5)
+#the ** operator raises a number to 
+print("In python, 10 ** 2 = ", 10**2)
+#the / operator is division
+print("In python, 15 / 10 = ", 15/10)
