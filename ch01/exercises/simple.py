aca589080073b75c1dfe3141864dc6af6441efa6
Jack Hunter <89477914+jhthirteen@users.noreply.github.com>
2022-09-06 21:42:50 +0000

    Completed Exercise 1: Working With Data

--- ch01/exercises/simple.py
+++ ch01/exercises/simple.py
@@ -1,11 +1,22 @@
+print("Part 1:")
 #the * operator is multiplication 
-print("In python, 10 * 5 = ", 10*5)
+print("10 * 5 = ", 10*5)
 #the ** operator raises a number to 
-print("In python, 10 ** 2 = ", 10**2)
+print("10 ** 2 = ", 10**2)
 #the / operator is division
-print("In python, 15 / 10 = ", 15/10)
-#the // operator is floor division(round down)
-print("In python, 15 // 10 = ", 15//10)
-print("In python, -15 // 10 = ", -15//10)
+print("15 / 10 = ", 15/10)
+#the // operator is interger division(round down)
+print("15 // 10 = ", 15//10)
+print("-15 // 10 = ", -15//10)
 #the % operator is modulus (prints remainder)
-print("In python, 15 % 10 = ", 15%10)
+print("15 % 10 = ", 15%10)
+print("10 % 15 = ", 10%15)
+print("10 % 10 = ", 10%10)
+#this causes a repeating decimal, the 6 will go on forever
+print("10 / 15 = ", 10/15)
+print("Part 2:")
+rate = float(input("Enter the current exchange rate for the Euro to US Dollar: "))
+amount = float(input("Enter the amount of currency to exchange: (There is a $3 service fee) "))
+total = rate*amount
+result = total-3
+print("You will receive $", result, " US Dollars")
