972ad09394554b4bb4b8204f18537226b1116124
Jack Hunter <89477914+jhthirteen@users.noreply.github.com>
2022-09-19 20:59:40 +0000

    finished ch3 exercise: guess a number and fixed README

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -1,12 +1,20 @@
 import random 
 
-target_num = random.randrange(0,10,1)
+target_num = random.randrange(1,11)
+num_guesses = 0
+right_wrong = False #flag to determine if the script should print right/wrong message
 for _ in range(3):
   user_guess = int(input("Guess a number 1-10: "))
+  num_guesses += 1
   if (user_guess > target_num):
-    print("Too High!")
+    print("Too High")
   elif(user_guess < target_num):
-    print("Too Low!")
+    print("Too Low")
   else:
-    print("Correct!")
+    print("Correct")
+    right_wrong = True 
     break
+if (right_wrong == False):
+  print("You did not guess the number right!")
+else: 
+  print("It took you", num_guesses, "to get it right!")
